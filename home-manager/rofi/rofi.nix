1be226aac0a86e8cd2afaa8af6da93df14324a86
FranBec <franbecvort@gmail.com>
2025-11-13 21:01:36 +0000

    rofi styling

--- home-manager/rofi/rofi.nix
+++ home-manager/rofi/rofi.nix
@@ -1,4 +1,7 @@
 { pkgs, config, ... }:
+let
+  rofiConfigDir = "${config.xdg.configHome}/rofi";
+in
 {
   home.packages = with pkgs; [
     rofi
@@ -7,18 +10,25 @@
   programs.rofi = {
     enable = true;
     
-    theme = "${config.xdg.configHome}/rofi/catppuccin-default.rasi"; 
+    theme = "${rofiConfigDir}/launcher-t1-s5.rasi"; 
 
-    extraConfig = {};
+    extraConfig = {
+      modi = "drun,run"; 
+    };
   };
 
-  home.file.".config/rofi/catppuccin-default.rasi" = {
-    source = ./catppuccin-default.rasi;
-    target = ".config/rofi/catppuccin-default.rasi";
+  home.file.".config/rofi/launcher-t1-s5.rasi" = {
+    source = ./launcher-t1-s5.rasi;
+    target = "${rofiConfigDir}/launcher-t1-s5.rasi";
   };
-  
+
   home.file.".config/rofi/catppuccin-mocha.rasi" = {
     source = ./catppuccin-mocha.rasi;
-    target = ".config/rofi/catppuccin-mocha.rasi";
+    target = "${rofiConfigDir}/catppuccin-mocha.rasi";
+  };
+  
+  home.file.".config/rofi/shared/colors.rasi" = {
+    source = ./shared/colors.rasi;
+    target = "${rofiConfigDir}/shared/colors.rasi";
   };
 }
