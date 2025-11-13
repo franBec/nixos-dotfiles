6da7dfb01fd59db82064fa4c18909f0508115ae3
FranBec <franbecvort@gmail.com>
2025-11-13 12:30:38 +0000

    renamed host

--- flake.nix
+++ flake.nix
@@ -17,14 +17,14 @@
             
             # IMPORTS NOW POINT TO THE HOST DIRECTORY
             modules = [
-                ./hosts/LENOVO-LNVNB161216/configuration.nix
+                ./hosts/Lenovo-ideapad-320-15IKB/configuration.nix
                 home-manager.nixosModules.home-manager
                 {
                     home-manager = {
                         useGlobalPkgs = true;
                         useUserPackages = true;
                         # Home Manager config for user pollito now points to the host-specific home.nix
-                        users.pollito = import ./hosts/LENOVO-LNVNB161216/home.nix;
+                        users.pollito = import ./hosts/Lenovo-ideapad-320-15IKB/home.nix;
                         backupFileExtension = "backup";
                     };
                 }
