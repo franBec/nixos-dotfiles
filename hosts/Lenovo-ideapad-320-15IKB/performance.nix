16f10ee63b22fa57f9267c6ed4b6be4fba106981
FranBec <franbecvort@gmail.com>
2025-11-14 13:17:58 +0000

    comments

--- hosts/Lenovo-ideapad-320-15IKB/performance.nix
+++ hosts/Lenovo-ideapad-320-15IKB/performance.nix
@@ -3,7 +3,13 @@
 {
   # 1. NVIDIA Hybrid Graphics Setup
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Note: Slightly older card (940MX) might sometimes need to use
+  # `legacyPackages.390` or `legacyPackages.470` instead of `stable`,
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  # NOTE: This setting makes the Intel GPU the primary display output.
+  # You must use `prime-run` to launch applications on the NVIDIA card.
   services.xserver.prime.enable = true;
 
   # 2. Performance Tweaks (Memory/I/O)
@@ -13,7 +19,6 @@
   services.zram.algorithm = "zstd";
 
   # Set default I/O scheduler via kernel parameters
-  # 'kyber' is a common choice for modern SSDs.
   boot.kernelParams = [
     "elevator=kyber"
   ];
