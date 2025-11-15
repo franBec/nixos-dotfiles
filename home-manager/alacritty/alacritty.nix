98c891727a34eadf715cd676f10af0b8ac04a22e
FranBec <franbecvort@gmail.com>
2025-11-15 13:05:06 +0000

    Autostart Alacritty on login - left side, centered vertically

--- home-manager/alacritty/alacritty.nix
+++ home-manager/alacritty/alacritty.nix
@@ -5,12 +5,13 @@
     settings = builtins.fromTOML (builtins.readFile ./alacritty.toml);
   };
 
+  # Autostart Alacritty on login - left side, centered vertically
   home.file.".config/autostart/alacritty.desktop".text = ''
     [Desktop Entry]
     Type=Application
     Name=Alacritty
     Comment=Terminal emulator
-    Exec=${pkgs.alacritty}/bin/alacritty
+    Exec=${pkgs.alacritty}/bin/alacritty --option window.dimensions.columns=120 --option window.dimensions.lines=35 --option window.position.x=50 --option window.position.y=220
     Icon=Alacritty
     Terminal=false
     Categories=System;TerminalEmulator;
