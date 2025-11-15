e6f6def2bb16031e2b7c578e088afb01fd803acc
FranBec <franbecvort@gmail.com>
2025-11-15 13:12:47 +0000

    alacritty window size

--- home-manager/alacritty/alacritty.nix
+++ home-manager/alacritty/alacritty.nix
@@ -5,13 +5,13 @@
     settings = builtins.fromTOML (builtins.readFile ./alacritty.toml);
   };
 
-  # Autostart Alacritty on login - left side, centered vertically
+  # Autostart Alacritty on login - top-left corner
   home.file.".config/autostart/alacritty.desktop".text = ''
     [Desktop Entry]
     Type=Application
     Name=Alacritty
     Comment=Terminal emulator
-    Exec=${pkgs.alacritty}/bin/alacritty --option window.dimensions.columns=120 --option window.dimensions.lines=35 --option window.position.x=50 --option window.position.y=220
+    Exec=${pkgs.alacritty}/bin/alacritty --option window.dimensions.columns=120 --option window.dimensions.lines=35 --option window.position.x=50 --option window.position.y=50
     Icon=Alacritty
     Terminal=false
     Categories=System;TerminalEmulator;
