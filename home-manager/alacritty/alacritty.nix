aa96c9a599d30c52cd93fdef9e338298eb85f1f2
FranBec <franbecvort@gmail.com>
2025-11-15 01:46:16 +0000

    Autostart Alacritty on login

--- home-manager/alacritty/alacritty.nix
+++ home-manager/alacritty/alacritty.nix
@@ -4,4 +4,17 @@
     enable = true;
     settings = builtins.fromTOML (builtins.readFile ./alacritty.toml);
   };
+
+  home.file.".config/autostart/alacritty.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Alacritty
+    Comment=Terminal emulator
+    Exec=${pkgs.alacritty}/bin/alacritty
+    Icon=Alacritty
+    Terminal=false
+    Categories=System;TerminalEmulator;
+    StartupNotify=false
+    X-GNOME-Autostart-enabled=true
+  '';
 }
