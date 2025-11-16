67c772e9e9a60db8f69212f2b4f5c0a42957473a
FranBec <franbecvort@gmail.com>
2025-11-16 15:48:55 +0000

    nix for loop

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,21 +1,29 @@
 { config, pkgs, ... }:
 
+let
+  # Modules following the ./something/something.nix pattern
+  simpleModules = [
+    "alacritty"
+    "ani-cli"
+    "audio-tools"
+    "btop"
+    "google-chrome"
+    "git"
+    "komorebi"
+    "rofi"
+    "shell-customization"
+    "ssh"
+  ];
+
+  simpleImports = map (name: ./${name}/${name}.nix) simpleModules;
+in
 {
   home.username = "pollito";
   home.homeDirectory = "/home/pollito";
   home.stateVersion = "25.05";
 
-  imports = [
-    ./alacritty/alacritty.nix
-    ./ani-cli/ani-cli.nix
-    ./btop/btop.nix
-    ./google-chrome/google-chrome.nix
-    ./git/git.nix
-    ./komorebi/komorebi.nix
+  imports = simpleImports ++ [
     ./performance-test/mesa-demos.nix
-    ./rofi/rofi.nix
-    ./shell-customization/shell-customization.nix
-    ./ssh/ssh.nix
     ./jetbrains/webstorm.nix
     ./utils.nix
   ];
