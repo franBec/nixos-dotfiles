500d4f7f02b4d3d6ea03877de6e4901c8095e846
FranBec <franbecvort@gmail.com>
2025-11-16 15:40:34 +0000

    alsa-utils

--- home-manager/audio-tools/audio-tools.nix
+++ home-manager/audio-tools/audio-tools.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    alsa-utils
     pavucontrol      # PulseAudio/PipeWire volume control GUI
     qpwgraph         # PipeWire graph manager (like JACK patchbay)
     helvum           # Alternative PipeWire patchbay (simpler)
