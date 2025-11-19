366fd35c09582a22e36cea3130d914f69338d7a7
FranBec <franbecvort@gmail.com>
2025-11-19 20:42:08 +0000

    musescore logo

--- home-manager/audio-tools/musescore/musescore.nix
+++ home-manager/audio-tools/musescore/musescore.nix
@@ -4,15 +4,14 @@
     appimage-run
   ];
 
-  # Desktop Entry
-  # This assumes the AppImage is /home/pollito/Applications/MuseScore.AppImage
+  home.file.".local/share/icons/musescore.png".source = ./musescore.png;
   home.file.".local/share/applications/musescore-appimage.desktop".text = ''
     [Desktop Entry]
     Type=Application
     Name=MuseScore 4
     Comment=Create, play and print beautiful sheet music
     Exec=${pkgs.appimage-run}/bin/appimage-run /home/pollito/Applications/MuseScore.AppImage %F
-    Icon=musescore
+    Icon=/home/pollito/.local/share/icons/musescore.png
     Terminal=false
     Categories=AudioVideo;Audio;Music;
     MimeType=application/x-musescore;application/x-musescore+xml;
