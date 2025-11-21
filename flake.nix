de97b2bf842d764fe607f020fb63bfad88b3bd37
FranBec <franbecvort@gmail.com>
2025-11-21 14:40:15 +0000

    with pkgs-unstable attempt

--- flake.nix
+++ flake.nix
@@ -1,33 +1,49 @@
 {
-    description = "NixOS flake";
-    inputs = {
-        nixpkgs.url = "nixpkgs/nixos-25.05";
+  description = "NixOS flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-        home-manager = {
-            url = "github:nix-community/home-manager/release-25.05";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+
+  let
+    system = "x86_64-linux";
+
+    # Define both package sets
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
 
-    outputs = {self, nixpkgs, home-manager, ...}: {
-
-        # Define configurations for specific hosts
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            
-            # IMPORTS NOW POINT TO THE HOST DIRECTORY
-            modules = [
-                ./hosts/Lenovo-ideapad-320-15IKB/configuration.nix
-                home-manager.nixosModules.home-manager
-                {
-                    home-manager = {
-                        useGlobalPkgs = true;
-                        useUserPackages = true;
-                        users.pollito = import ./home-manager/home.nix;
-                        backupFileExtension = "backup";
-                    };
-                }
-            ];
-        };
+    pkgs-unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
     };
+
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      modules = [
+        ./hosts/Lenovo-ideapad-320-15IKB/configuration.nix
+        home-manager.nixosModules.home-manager
+
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.pollito = import ./home-manager/home.nix {
+              inherit pkgs pkgs-unstable;
+            };
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
 }
