227cfe68578ccdf1ee999ce540afd74ddc9a0742
FranBec <franbecvort@gmail.com>
2025-11-28 15:39:30 +0000

    azure ssh key

--- home-manager/ssh/ssh.nix
+++ home-manager/ssh/ssh.nix
@@ -8,6 +8,11 @@
         identityFile = "~/.ssh/github_ed25519";
         forwardAgent = false;
       };
+      "ssh.dev.azure.com" = {
+        user = "git";
+        identityFile = "~/.ssh/azuredevops_rsa";
+        forwardAgent = false;
+      };
     };
   };
 }
