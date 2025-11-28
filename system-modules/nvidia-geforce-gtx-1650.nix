2fea38982d59fbc3427b1f319b36f3673a6cc0d3
FranBec <franbecvort@gmail.com>
2025-11-28 00:40:10 +0000

    removed the Windows spoofing lines (acpi_osi) since they didn't succeed in tricking the BIOS, and it is better to run standard Linux ACPI tables if the spoof doesn't provide a tangible benefit.

--- system-modules/nvidia-geforce-gtx-1650.nix
+++ system-modules/nvidia-geforce-gtx-1650.nix
@@ -6,17 +6,15 @@
    vulkan-tools
    lm_sensors
   ];
-  boot.kernelParams = [
-    "pcie_aspm=force"
-    "acpi_osi=!"
-    "acpi_osi=\"Windows 2009\""
-  ];
-  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
-  boot.extraModprobeConfig = ''
-    options nvidia "NVreg_DynamicPowerManagement=0x02"
-    options nvidia_drm modeset=1
-  '';
-  services.thermald.enable = true;
+
+  boot = {
+    initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+    kernelParams = [ "pcie_aspm=force" ];
+    extraModprobeConfig = ''
+      options nvidia "NVreg_DynamicPowerManagement=0x02"
+      options nvidia_drm modeset=1
+    '';
+  };
   hardware = {
     graphics = {
       enable = true;
@@ -26,9 +24,11 @@
       modesetting.enable = true;
       nvidiaSettings = true;
       open = false;
-      powerManagement.enable = true;
-      powerManagement.finegrained = true;
       package = config.boot.kernelPackages.nvidiaPackages.production;
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
       prime = {
         intelBusId  = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
@@ -39,10 +39,13 @@
       };
     };
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
+  services = {
+    thermald.enable = true;
+    udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
+    xserver.videoDrivers = [ "nvidia" ];
+  };
 }
