e4d325f2d54fbded7fce67885b43a9555e22998c
FranBec <franbecvort@gmail.com>
2025-11-28 22:04:53 +0000

    no mangohud

--- home-manager/steam/steam.nix
+++ home-manager/steam/steam.nix
@@ -1,9 +1,6 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    mangohud
-  ];
   programs.gamemode.enable = true;
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
