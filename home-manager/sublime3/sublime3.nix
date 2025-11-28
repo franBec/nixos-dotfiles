b047d9b24d61e37de3958fb86172d43678551372
FranBec <franbecvort@gmail.com>
2025-11-28 15:51:15 +0000

    sublime-merge

--- home-manager/sublime3/sublime3.nix
+++ home-manager/sublime3/sublime3.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     sublime3
-    sublime_merge
+    sublime-merge
   ];
 
   home.file.".config/sublime-text-3/Packages/User/Adapta-Nokto.sublime-color-scheme".text = ''
