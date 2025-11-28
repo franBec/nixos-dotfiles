b4a5f3c10d9a46884a7a21647a5f65d6778cb2ca
FranBec <franbecvort@gmail.com>
2025-11-28 22:04:13 +0000

    steam x2

--- home-manager/steam/steam.nix
+++ home-manager/steam/steam.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }:
+
 {
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+  programs.gamemode.enable = true;
   programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
 }
