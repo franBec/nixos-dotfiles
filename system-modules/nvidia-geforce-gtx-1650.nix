aa40109442825ad5d1126705d28b02ec35010e00
FranBec <franbecvort@gmail.com>
2025-11-28 00:24:09 +0000

    fixing Lenovo ACPI bugs

--- system-modules/nvidia-geforce-gtx-1650.nix
+++ system-modules/nvidia-geforce-gtx-1650.nix
@@ -6,16 +6,17 @@
    vulkan-tools
    lm_sensors
   ];
-
- boot.kernelParams = [
+  boot.kernelParams = [
     "pcie_aspm=force"
     "acpi_osi=!"
-    "acpi_osi=\"Windows 2015\""
+    "acpi_osi=\"Windows 2009\""
   ];
+  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
   boot.extraModprobeConfig = ''
     options nvidia "NVreg_DynamicPowerManagement=0x02"
+    options nvidia_drm modeset=1
   '';
-
+  services.thermald.enable = true;
   hardware = {
     graphics = {
       enable = true;
@@ -25,9 +26,9 @@
       modesetting.enable = true;
       nvidiaSettings = true;
       open = false;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
       powerManagement.enable = true;
       powerManagement.finegrained = true;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
       prime = {
         intelBusId  = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
@@ -38,11 +39,10 @@
       };
     };
   };
-  services.thermald.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
   '';
-  services.xserver.videoDrivers = [ "nvidia" ];
 }
