1639e1d8b84143dfe0f95dba0849d858ca650fde
FranBec <franbecvort@gmail.com>
2025-11-29 00:32:26 +0000

    sox utility

--- home-manager/audio-utils/audio-utils.nix
+++ home-manager/audio-utils/audio-utils.nix
@@ -6,5 +6,6 @@
     qpwgraph         # PipeWire graph manager (like JACK patchbay)
     helvum           # Alternative PipeWire patchbay (simpler)
     easyeffects      # Audio effects (EQ, compressor, etc.)
+    sox
   ];
 }
