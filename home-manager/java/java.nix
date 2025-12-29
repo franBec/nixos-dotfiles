404b78ae48ac88614cc6167ddc1d4b6682084beb
FranBec <franbecvort@gmail.com>
2025-12-29 14:50:29 +0000

    downgrade to jdk21

--- home-manager/java/java.nix
+++ home-manager/java/java.nix
@@ -1,11 +1,11 @@
 { pkgs, pkgs-unstable, ... }:
 {
   home.packages = [
-    pkgs-unstable.javaPackages.compiler.openjdk25
+    pkgs-unstable.javaPackages.compiler.openjdk21
     pkgs-unstable.gradle_9
   ];
 
   home.sessionVariables = {
-    JAVA_HOME = "${pkgs-unstable.javaPackages.compiler.openjdk25}/lib/openjdk";
+    JAVA_HOME = "${pkgs-unstable.javaPackages.compiler.openjdk21}/lib/openjdk";
   };
 }
